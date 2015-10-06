bb4ad607a278e797e718f91f484a80c023d12319
Rickard Andersson <gonz@severnatazvezda.com>
2015-10-06 13:51:01 +0300

    Add HD usage stats to output

--- space-check.sh
+++ space-check.sh
@@ -6,5 +6,5 @@ NUMENTRIES=$2
 
 [[ -z $FOLDER ]] && FOLDER="."
 [[ -z $NUMENTRIES ]] && NUMENTRIES="15"
-
+df -hT | gawk -e '!/tmpfs/ && !/^Filesystem/ { print $1,"("$7")",$5"/"$3,"("(100 - $6)"%)","free" }'
 du -h $FOLDER | sort -k1rh | head -n $NUMENTRIES
